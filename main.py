c78724a1a6420a209834efce7d20c4aa17579afb
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-07-12 21:08:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import translators
 
+q_text = "Hello, how are you?"
+
+print(translators.translate_text(q_text, translator="google", to_language="hi"))
+
+print(translators.translate_text(q_text, translator="google", to_language="zh"))
